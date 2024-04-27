c3bf3a99bf732f0fa9ace0c1a699f8bb488bb292
cheeseOFcheese <96741794+cheeseOFcheese@users.noreply.github.com>
2024-04-28 01:23:43 +0300

    Create Code.py

---
+++ visualization Blender3D /Code.py
@@ -0,0 +1,102 @@
+import bpy
+import numpy as np
+import random
+
+# Удаляем все объекты из сцены
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete()
+
+# Определим матрицу совместимости и вектор весов
+compatibility_matrix = np.array([
+   [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],  
+])
+
+weight_vector = [
+    6.7871, 0.4965, 0.5813, 6.3638, 7.7534, 6.7388, 2.1945, 5.7203,
+    # Другие значения
+]
+
+# Создание материала
+def create_material(name, color):
+    material = bpy.data.materials.new(name=name)
+    material.use_nodes = True
+    bsdf = material.node_tree.nodes.get("Principled BSDF")
+    bsdf.inputs['Base Color'].default_value = color
+    return material
+
+# Создание куба
+def create_cube(location, scale, material):
+    bpy.ops.mesh.primitive_cube_add(
+        size=1,
+        enter_editmode=False,
+        align='WORLD',
+        location=location
+    )
+    cube = bpy.context.active_object
+    cube.scale = scale
+    cube.data.materials.append(material)
+    return cube
+
+# Определение размеров сетки
+num_rows = len(compatibility_matrix)
+num_columns = len(compatibility_matrix[0])
+
+# Создание кубиков на основе матрицы
+for row_index in range(num_rows):
+    for col_index in range(num_columns):
+        # Позиция кубика
+        location = (col_index * 2, row_index * 2, 0)  
+        
+        # Определение высоты на основе веса и матрицы
+        base_height = weight_vector[row_index]
+        height_multiplier = 1 + compatibility_matrix[row_index][col_index]  # Прибавляем 1 при 1 в матрице
+        scale = (1, 1, base_height * height_multiplier)  # Управляем высотой
+        
+        # Определение цвета на основе матрицы
+        if compatibility_matrix[row_index][col_index] == 1:
+            color = (0, 1, 0, 1)  # Зеленый
+        else:
+            color = (1, 0, 0, 1)  # Красный
+        
+        # Создаем материал
+        material = create_material(f'Mat_{row_index}_{col_index}', color)
+        
+        # Создаем куб
+        cube = create_cube(location, scale, material)
+        
+        # Анимация для "летающих" кубов
+        frame_start = 1
+        frame_end = 100
+        
+        cube.keyframe_insert(data_path='location', frame=frame_start)  # Начало
+        
+        # Взлет на основе матрицы
+        base_lift = random.uniform(10, 20)  # Базовое значение
+        lift_multiplier = 1 + compatibility_matrix[row_index][col_index]  # Увеличиваем при 1 в матрице
+        cube.location[2] = base_lift * lift_multiplier  # Конечное положение по Z
+        cube.keyframe_insert(data_path='location', frame=frame_end)  # Конец
